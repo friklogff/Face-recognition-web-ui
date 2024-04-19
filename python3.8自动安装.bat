3316125ebc13d2249d0c321d4a647d5570fa4d50
friklogff <98448908+friklogff@users.noreply.github.com>
2024-04-19 13:20:04 +0800

    Update python3.8自动安装.bat

--- python3.8自动安装.bat
+++ python3.8自动安装.bat
@@ -1,7 +1,19 @@
-chcp 65001
 @echo off
-echo Python未安装,开始下载安装包
-curl -O https://mirrors.huaweicloud.com/python/3.7.0/python-3.7.0-amd64.exe
-echo 安装包下载完成,开始安装
-python-3.7.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+chcp 65001
+
+REM 定义Python版本和下载链接
+set PYTHON_VERSION=3.8.0
+set PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
+
+REM 下载Python安装程序
+echo 下载Python安装程序...
+curl -o python-%PYTHON_VERSION%-amd64.exe %PYTHON_DOWNLOAD_URL%
+
+REM 安装Python
+echo 安装Python %PYTHON_VERSION%...
+python-%PYTHON_VERSION%-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+
+REM 删除下载的安装程序
+del python-%PYTHON_VERSION%-amd64.exe
+
 pause
